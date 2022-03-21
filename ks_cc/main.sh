2146a4e9aa3eff6b38d4e2b93231d45d3f5f4cea
Brian <brianli.li@mail.utoronto.ca>
2022-03-21 16:08:59 -0400

    upload cc pipeline

---
+++ ks_cc/main.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Ensure parameters are specified on the command-line
+
+[ "$1" = "-h" -o "$1" = "--help" ] && echo "
+Description:
+    This is a driver script that handles generating the SAT encoding, generating non-canonical subgraph blocking clauses,
+    simplify instance using CaDiCaL, solve the instance using maplesat-ks, then finally determine if a KS system exists for a certain order.
+
+Usage:
+    ./main.sh n s r
+
+Options:
+    <n>: the order of the instance/number of vertices in the graph
+    <s>: number of times to simplify the instance using CaDiCaL with default value 3
+    <r>: number of variable to remove in cubing, if not passed in, assuming no cubing needed
+" && exit
+
+#step 1: input parameters
+if [ -z "$1" ]
+then
+    echo "Need instance order (number of vertices) and number of simplification, use -h or --help for further instruction"
+    exit
+fi
+
+n=$1 #order
+s=${2:-3} #number of time to simplify each to simplification is called
+r=${3:-0} #number of variables to eliminate until the cubing terminates
+
+#step 2: setp up dependencies
+./dependency-setup.sh
+
+#step 3: generate instances
+./1-instance-generation.sh $n
+
+#simplify s times
+if [ -f constraints_$n.simp1 ]
+then
+    echo "constraints_$n.simp1 already exist, skip simplification"
+else
+    ./simplification/simplify.sh constraints_$n $s
+    mv constraints_$n.simp constraints_$n.simp1
+fi
+
+#simplify s times again
+if [ -f constraints_$n.simp2 ]
+then
+    echo "constraints_$n.simp2 already exist, skip simplification"
+else
+    ./2-add-blocking-clauses.sh $n 12 constraints_$n.simp1
+    ./simplification/simplify.sh constraints_$n.simp1 $s
+    mv constraints_$n.simp1.simp constraints_$n.simp2
+fi
+
+if [[ $(wc -l <constraints_$n.simp2) -ge 3 ]] #if the constraint file only has two lines or less after simplification, output unsat
+then 
+    #step 5: cube and conquer if necessary, then solve
+
+    #everything after this line should be done on compute canada through slurm
+    if [ "$r" != "0" ] 
+    then 
+        ./3-cube-merge-solve.sh $n $r constraints_$n.simp2 #need to sbatch this
+    else
+        ./maplesat-ks/simp/maplesat_static constraints_$n.simp2 -no-pre -exhaustive=$n.exhaust -order=$n
+    fi
+
+    #step 6: checking if there exist embeddable solution should move to local
+
+    #output the number of KS system is there is any
+fi
---
+++ ks_cc/simplification/simplify-by-var-removal.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#set -x
+
+[ "$1" = "-h" -o "$1" = "--help" ] && echo "
+Description:
+    This script simplify the instance, add back edge blocking clauses, then repeat
+	until a certain percentage of variables are eliminated
+
+Usage:
+    ./simplify-by-var-removal f p m
+
+Options:
+	<n>: the order of the instance/number of vertices in the graph
+    <f>: file name of the current SAT instance
+    <p>: percentage of variables to eliminate (1-100)
+" && exit
+
+# Ensure parameters are specified on the command-line
+if [ -z "$2" ]
+then
+	echo "Need filename and the percentage of variables to remove"
+	exit
+fi
+
+n=$1
+f="$2"
+p="$3"
+m=$((n*(n-1)/2))
+
+sub_dir=$(echo "${f%%/*}")
+#Directory to log simplification output
+mkdir -p log/$sub_dir
+
+# Directory for simplified output
+mkdir -p simp/$sub_dir
+
+# Simplify until at least p percent of variables have been removed
+./cadical/build/cadical "$f" -o simp/"$f".simp1 -e simp/"$f".ext1 -n -c 200000 | tee log/"$f".simp1
+for i in $(seq 1 100)
+do
+	vars_remaining=$(grep "c [*]" log/"$f".simp"$i" | tail -n 1 | rev | cut -d' ' -f1 | rev | sed 's/.$//')
+	if (( vars_remaining < 100 - p ))
+	then
+		break
+	fi
+	numvars=$(head -n 1 simp/"$f".simp"$i" | cut -d' ' -f3)
+	numlines1=$(head -n 1 simp/"$f".simp"$i" | cut -d' ' -f4)
+	numlines2=$(awk "sqrt(\$(NF-1)*\$(NF-1))<=$m" "simp/"$f".ext$i" | wc -l | cut -d' ' -f1)
+	newlines=$((numlines1+numlines2))
+	var="p cnf $numvars $newlines"
+	sed -i "1s/.*/$var/" simp/"$f".simp"$i" #rewrite the first line
+	awk "sqrt(\$(NF-1)*\$(NF-1))<=$m" "simp/"$f".ext$i" | sed 's/ 0.*/ 0/' >> simp/"$f".simp"$i"
+	./cadical/build/cadical simp/"$f".simp"$i" -o simp/"$f".simp$((i+1)) -e simp/"$f".ext$((i+1)) -n -c 200000 | tee log/"$f".simp$((i+1))
+done
+
+# Number of variables and lines in the final simplified instance
+numvars=$(head -n 1 simp/"$f".simp"$i" | cut -d' ' -f3)
+numlines=$(head -n 1 simp/"$f".simp"$i" | cut -d' ' -f4)
+
+# Lines in reconstruction stack
+extlines_total=0
+for j in $(seq 1 "$i")
+do
+	extlines_j=$(awk "sqrt(\$(NF-1)*\$(NF-1))<=$m" "simp/"$f".ext$j" | wc -l | cut -d' ' -f1)
+	extlines_total=$((extlines_total+extlines_j))
+done
+
+# Lines in simplified instance with reconstruction stack
+newlines=$((numlines+extlines_total))
+
+# Output final simplified instance
+echo "p cnf $numvars $newlines" > "$f".simp
+tail simp/"$f".simp"$i" -n +2 >> "$f".simp
+for j in $(seq 1 "$i")
+do
+	awk "sqrt(\$(NF-1)*\$(NF-1))<=$m" "simp/"$f".ext$j" | sed 's/ 0.*/ 0/' >> "$f".simp
+done
+sed -i 's/ 0.*/ 0/' "$f".simp
