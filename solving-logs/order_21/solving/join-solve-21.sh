d3b4ba4e48470cbb8d9a84045ba8be5a00c2288b
BrianLi009 <brianli.li@mail.utoronto.ca>
2022-09-03 21:22:33 -0400

    add cc script

--- solving-logs/order_21/solving/join-solve-21.sh
+++ solving-logs/order_21/solving/join-solve-21.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --array=287,358,364,394,401,483
+#SBATCH --array={specify the cubes to be solved}
 #SBATCH --time=60:00:00
-#SBATCH --account=def-vganesh
+#SBATCH --account={account name}
 #SBATCH --mem=2G
 ./simplification/adjoin-cube-simplify.sh 21 constraints_21_60_2_2.simp2 23.cubes $SLURM_ARRAY_TASK_ID 50
 ./maplesat-ks/simp/maplesat_static 23.cubes$SLURM_ARRAY_TASK_ID.adj.simp -no-pre -exhaustive=21-$SLURM_ARRAY_TASK_ID.exhaust -order=21
