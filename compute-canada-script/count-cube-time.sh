1866015413e46fdbfd38b3adf23a5cc12a103d25
BrianLi009 <brianli.li@mail.utoronto.ca>
2022-09-12 20:12:28 -0400

    runtime comp script

---
+++ compute-canada-script/count-cube-time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+f=$1
+
+echo $f
+
+touch "$f"_count.log
+
+grep 'c time' $f | cut -d = -f 2 | cut -d ' ' -f 2 >> "$f"_count.log
+
+awk '{s+=$1} END {print s}' "$f"_count.log
---
+++ compute-canada-script/count-simp-time.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+i=$1
+
+touch "$i"_count.log
+
+total_time=0
+
+for f in $i/slurm*.out
+do 	
+	grep 'c total real time since initialization:' $f | cut -d = -f 2 | cut -d ':' -f 2 | cut -d 's' -f 1 >> "$i"_count.log
+done
+
+awk '{s+=$1} END {print s}' "$i"_count.log
