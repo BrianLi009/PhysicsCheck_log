477288ae73384c0981e6edca7fab4bec196b1d10
BrianLi009 <brianli.li@mail.utoronto.ca>
2023-06-01 20:58:10 -0400

    update

--- compute-canada-script/3-cube-merge-solve-iterative-learnt-cc.sh
+++ compute-canada-script/3-cube-merge-solve-iterative-learnt-cc.sh
@@ -34,19 +34,21 @@ for i in $(seq 1 $new_index) #1-based indexing for cubes
         child_instance="$d/$v/simp/${highest_num}.cubes${i}.adj.simp"
         command1="./gen_cubes/apply.sh $f $cube_file $i > $d/$v/simp/$cube_file$i.adj"
         command2="./simplification/simplify-by-conflicts.sh $d/$v/simp/$cube_file$i.adj $n $t >> $d/$v/$n-solve/$i-solve.log"
-        command3="./maplesat-solve-verify.sh -l $n $d/$v/simp/$cube_file$i.adj.simp $d/$v/$n-solve/$i-solve.exhaust >> $d/$v/$n-solve/$i-solve.log"
-        command4="if ! grep -q 'UNSATISFIABLE' '$d/$v/$n-solve/$i-solve.log'; then sbatch $child_instance-solve.sh; fi"
+        command3="timeout ${s}s ./maplesat-solve-verify.sh -l $n $d/$v/simp/$cube_file$i.adj.simp $d/$v/$n-solve/$i-solve.exhaust >> $d/$v/$n-solve/$i-solve.log"
+        command4="if ! grep -q 'UNSATISFIABLE' '$d/$v/$n-solve/$i-solve.log'; then sbatch $child_instance-cube.sh; fi"
         #sbatch this line
         command5="./gen_cubes/concat.sh $child_instance $child_instance.noncanonical > $child_instance.temp; ./gen_cubes/concat.sh $child_instance.temp $child_instance.unit > $child_instance.learnt; ./3-cube-merge-solve-iterative-learnt-cc.sh $n $child_instance.learnt '$d/$v-$i' $(($v + $a)) $t $s $a $(($highest_num+2)) $new_cube_file"
-        command="$command1 && $command2 && $command3 && $command4"
+        command="$command1 && $command2 && $command3"
         echo "#!/bin/bash" > $child_instance-solve.sh
-        echo "#SBATCH --account=def-vganesh" >> $child_instance-solve.sh
-        echo "#SBATCH --time=0-08:00" >> $child_instance-solve.sh
-        echo "#SBATCH --mem-per-cpu=2G" >> $child_instance-solve.sh
+        echo "#SBATCH --account=def-cbright" >> $child_instance-solve.sh
+        echo "#SBATCH --time=0-04:00" >> $child_instance-solve.sh
+        echo "#SBATCH --mem-per-cpu=4G" >> $child_instance-solve.sh
         echo "#!/bin/bash" > $child_instance-cube.sh
-        echo "#SBATCH --account=def-vganesh" >> $child_instance-cube.sh
-        echo "#SBATCH --time=0-08:00" >> $child_instance-cube.sh
-        echo "#SBATCH --mem-per-cpu=2G" >> $child_instance-cube.sh
+        echo "#SBATCH --account=def-cbright" >> $child_instance-cube.sh
+        echo "#SBATCH --time=0-04:00" >> $child_instance-cube.sh
+        echo "#SBATCH --mem-per-cpu=4G" >> $child_instance-cube.sh
         echo $command >> $child_instance-solve.sh
+        echo $command4 >> $child_instance-solve.sh
         echo $command5 >> $child_instance-cube.sh
+        sbatch $child_instance-solve.sh
     done
